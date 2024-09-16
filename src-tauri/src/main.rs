b3da7e15b992443e188e8a4c731b69997e238930
cluser404 <30370919+cluser404@users.noreply.github.com>
2024-09-17 02:49:09 +0600

    added json parsing of vector3 array and passed via command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,8 @@
+use std::env;
+use std::process::Command;
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -8,8 +11,26 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-fn print(coordinates: &str) {
-    println!("{}", coordinates);
+fn print(data: &str) {
+    println!("{}", data);
+    
+    let output = if cfg!(target_os = "windows") {
+        Command::new("fluxprint.bat")
+            .args(["--data", &data.replace("\\", "\\\\").replace("\"", "\\\"")])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("fluxprint.sh")
+            .args(["--data", data])
+            .output()
+            .expect("failed to execute process")
+    };
+
+    if output.status.success() {
+        println!("{}", String::from_utf8_lossy(&output.stdout).trim());
+    } else {
+        eprintln!("Error {}", String::from_utf8_lossy(&output.stderr).trim());
+    }
 }
 
 fn main() {
